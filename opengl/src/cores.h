6bd5d169b6a838b4ac543630921c15e7418b34fc
Bosshentai <baptistamhernani@gmail.com>
2022-11-01 18:04:15 -0100

    learning old opengl

---
+++ opengl/src/cores.h
@@ -0,0 +1,14 @@
+#pragma once
+typedef float color[3];
+
+color red = {0.85, 0.12, 0.0};
+color green = {0.0, 1.0, 0.0};
+color blue = {0.0, 0.15, 0.35};
+color black = {0.0, 0.0, 0.0};
+color white = {1.0, 1.0, 1.0};
+color white_Snow = {0.88,0.91,0.89};
+color yellow = {1.0, 1.0, 0.0};
+color purple = {0.54, 0.17, 0.88};
+color grey = {0.8, 0.8, 0.8};
+color grey_light = {0.67,0.67,0.67};
+color orange = {1.0, 0.6, 0.2};
